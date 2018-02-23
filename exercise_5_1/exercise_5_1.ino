26bedba487cfd506aaf1973843a68ea479dee283
SimonQQQ <gussheqi@student.gu.se>
2018-02-23 05:11:28 +0100

    some changes based on TA's advise.

--- exercise_5_1/exercise_5_1.ino
+++ exercise_5_1/exercise_5_1.ino
@@ -5,26 +5,29 @@ Group Number: 07
 Members That contributed:
 Qing Lin
 Qiu Shengzhi
-Demonstration code: 
+Demonstration code: 44466
 
 ====================================== */
 
 // Pins for light up segments on the LED display
-#define A 11
-#define B 10
-#define C 7
-#define D 9
-#define E 8
-#define F 12
-#define G 13
+#define A 7
+#define B 6
+#define C 13
+#define D 3
+#define E 2
+#define F 4
+#define G 5
 // Pins for driving common anodes
-#define CA1 6
-#define CA2 5
+#define CA1 9
+#define CA2 8
 // Pins for A B C D E F G
 const int segs[7] = {A,B,C,D,E,F,G};
 // Segments for each number
-const byte numbers[10] = {0b11110111, 0b1100000, 0b1011011, 0b1111010, 0b1101100, 
-0b0111110, 0b0111111, 0b1110000, 0b1111111, 0b1111100}
+//const byte numbers[10] = {0b11110111, 0b1100000, 0b1011011, 0b1111010, 0b1101100, 
+//0b0111110, 0b0111111, 0b1110000, 0b1111111, 0b1111100};
+
+const byte numbers[10] = {0b00111111, 0b0000110, 0b01011011, 0b1001111, 0b1100110, 0b1101101,
+0b1111101, 0b0000111, 0b1111111, 0b1101111 };
 
 int maxTemp = 0;
 
@@ -41,7 +44,7 @@ void setup(){
 	pinMode(CA2, OUTPUT);
 	
 	pinMode(A0, INPUT); // Configuring pin A0 as input
-	pinMode( 4, INPUT); // digitalWrite(4,HIGH) for 2 bits dip switch
+	pinMode( 10, INPUT); // digitalWrite(4,HIGH) for 2 bits dip switch
 	Serial.begin(9600);	
 }
 
@@ -65,12 +68,18 @@ void ledSegments(byte number){
 }
 
 void loop(){
-	if(digitalRead(4)=0){
-		int temp = (analogRead(A0))*500.0)/1024;
-		if(temp > maxTemp) 
-			maxTemp = temp;
+  int temp = (analogRead(A0)*500.0)/1023;
+  if(temp > maxTemp) 
+    maxTemp = temp;
 
-		unsigned long startTime = millis();
+  Serial.print("Temp:");
+  Serial.println(temp);
+  Serial.print("Max:");
+  Serial.println(maxTemp);
+  Serial.println(digitalRead(10));
+  unsigned long startTime = millis();
+	
+	if(digitalRead(10)==0){
 		for (unsigned long elapsed = 0; elapsed < 600; elapsed = millis() - startTime ){
 			ledDigit1(numbers[(temp%100)/10]);
 			delay(5);
@@ -80,7 +89,6 @@ void loop(){
 	}
 	
 	else{
-		unsigned long startTime = millis();
 		for(unsigned elapsed = 0; elapsed < 600; elapsed = millis() - startTime){
 			ledDigit1(numbers[(maxTemp%100)/10]);
 			delay(5);
